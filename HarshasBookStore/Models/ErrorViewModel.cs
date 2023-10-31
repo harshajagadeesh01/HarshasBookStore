130d5bcde801d7b7c0c05f58deb1db84c23d1577
W0810531 <W0810531@ADL-A1201I-11>
2023-10-30 20:34:02 -0400

    Add project files.

---
+++ HarshasBookStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HarshasBookStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
