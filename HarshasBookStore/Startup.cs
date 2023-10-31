b7aff2c03faabec3a254f85fa89de674e130f97a
W0810531 <W0810531@ADL-A1201I-11>
2023-10-30 21:50:19 -0400

    drop down menu

--- HarshasBookStore/Startup.cs
+++ HarshasBookStore/Startup.cs
@@ -32,7 +32,7 @@ namespace HarshasBookStore
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
         }
